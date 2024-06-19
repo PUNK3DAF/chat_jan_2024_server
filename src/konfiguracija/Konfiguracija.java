97501077a7f49456603eba3dfa486a897e59d070
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-06-19 16:49:52 +0200

    Dopuna konfiguracije

--- src/konfiguracija/Konfiguracija.java
+++ src/konfiguracija/Konfiguracija.java
@@ -4,10 +4,47 @@
  */
 package konfiguracija;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author vldmrk
  */
 public class Konfiguracija {
+
+    private static Konfiguracija instance;
+    private Properties konfiguracija;
+    String putanja = "C:\\Users\\vldmrk\\OneDrive - Fakultet organizacionih nauka\\Documents\\NetBeansProjects\\CHATJAN2024_SERVER\\config\\app.config";
+
+    private Konfiguracija() {
+        konfiguracija = new Properties();
+        File configFile = new File(putanja);
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdirs();
+            konfiguracija.setProperty("max_broj_klijenata", 5 + "");
+        } else {
+            try {
+                FileInputStream fis = new FileInputStream(configFile);
+                konfiguracija.load(fis);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Konfiguracija.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Konfiguracija.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public static Konfiguracija getInstance() {
+        if (instance == null) {
+            instance = new Konfiguracija();
+        }
+        return instance;
+    }
 
 }
